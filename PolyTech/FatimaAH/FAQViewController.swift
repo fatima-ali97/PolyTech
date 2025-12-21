e9d2e23d2068d8b922c21f7c5cc6a7df26db20b2
fatimaAhmed26 <fatima.bh2248@gmail.com>
2025-12-21 14:27:24 +0300

    edit

--- PolyTech/FatimaAH/FAQViewController.swift
+++ PolyTech/FatimaAH/FAQViewController.swift
@@ -4,29 +4,89 @@
 //
 //  Created by BP-19-130-12 on 21/12/2025.
 //
-
 import UIKit
 
-class FAQViewController: UIViewController {
+struct FAQItem {
+    let question: String
+    let answer: String
+    var isExpanded: Bool
+}
+
+final class FAQViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var faqs: [FAQItem] = [
+        FAQItem(question: "Banner login",
+                answer: "1) Go to the Bahrain Polytechnic website.\n2) Click Banner.\n3) Enter your student ID and password.",
+                isExpanded: false),
+
+        FAQItem(question: "Authenticator App setup",
+                answer: "1) Install Google Authenticator.\n2) Scan the QR code.\n3) Enter the 6-digit code to confirm.",
+                isExpanded: false),
+
+        FAQItem(question: "VMware virtual machine Starting Error",
+                answer: "1) Open VMware.\n2) Check the VM settings.\n3) Make sure the VM files path is correct.",
+                isExpanded: false),
+
+        FAQItem(question: "Password reset on computer",
+                answer: "1) Open the reset page.\n2) Enter your student ID.\n3) Follow the steps to set a new password.",
+                isExpanded: false),
+
+        FAQItem(question: "Moodle login",
+                answer: "1) Go to Moodle.\n2) Enter your credentials.\n3) If locked, reset your password.",
+                isExpanded: false)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "FAQs"
 
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBOutlet weak var SearchBar: UISearchBar!
-    
-    @IBAction func GetHelpbtn(_ sender: UIButton) {
-    }
-    /*
-    // MARK: - Navigation
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchBar.delegate = self
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        tableView.estimatedRowHeight = 60
+        tableView.rowHeight = UITableView.automaticDimension
     }
-    */
+}
 
+extension FAQViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return faqs.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Use a Subtitle cell style OR add your own labels
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FAQCell", for: indexPath)
+
+        let item = faqs[indexPath.row]
+        cell.textLabel?.text = item.question
+        cell.textLabel?.numberOfLines = 1
+
+        // Show answer only when expanded
+        cell.detailTextLabel?.text = item.isExpanded ? item.answer : nil
+        cell.detailTextLabel?.numberOfLines = 0
+
+        // Chevron
+        let iconName = item.isExpanded ? "chevron.up" : "chevron.down"
+        cell.accessoryView = UIImageView(image: UIImage(systemName: iconName))
+
+        cell.selectionStyle = .none
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        faqs[indexPath.row].isExpanded.toggle()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
+
+extension FAQViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // optional later (filtering)
+    }
 }
