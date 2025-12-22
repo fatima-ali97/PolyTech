c269db26c8ad557dea4f1c975a569734b54e68bf
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-12-22 14:45:46 +0300

    added some code in new maintenance request

--- PolyTech/MalakStoryboard/NewMaintenanceViewController.swift
+++ PolyTech/MalakStoryboard/NewMaintenanceViewController.swift
@@ -1,29 +1,51 @@
-//
-//  NewMaintenanceViewController.swift
-//  PolyTech
-//
-//  Created by BP-19-130-11 on 21/12/2025.
-//
-
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
 
-class NewMaintenanceViewController: UIViewController {
+class NewMaintenanceViewController: UIViewController, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var requestName: UITextField!
+    @IBOutlet weak var category: UITextField!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var urgency: UITextField!
+    @IBOutlet weak var imageUpload: UITextField!
+    
+    let database = Firestore.firestore()
+    let storage = Storage.storage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Savebtn(_ sender: UIButton) {
+        
     }
-    */
+    
+    let category = category.text ?? ""
+    let location = location.text ?? ""
+    let urgency = urgency.text ?? ""
 
+    let data: [String: Any] = [
+        "requestName": requestName,
+        "category": category,
+        "location": location,
+        "urgency": urgency,
+        "createdAt": Timestamp()
+    ]
+    
+    database.collection("maintenanceRequest").addDocument(data: data) { [weak self] error in
+            guard let self = self else { return }
+            if error == nil {
+                let alert = UIAlertController(title: "Success", message: "Maintenance request saved successfully", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                    self.navigationController?.popViewController(animated: true)
+                })
+                self.present(alert, animated: true)
+            } else {
+                print("Error: \(error!.localizedDescription)")
+            }
+
+
+    }
 }
