65c4a5ee886e9f9d2bb2dd2fcd173ecf907180a9
Ali <202305359@student.polytechnic.bh>
2025-12-24 17:03:48 +0300

    added more backend with the help of god the mighty the mercyful

--- PolyTech/TechnicianCardCell.swift
+++ PolyTech/TechnicianCardCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 final class TechnicianCardCell: UITableViewCell {
     
+    @IBOutlet weak var hoursViewCard: UIView!
+    @IBOutlet weak var tasksViewCard: UIView!
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var hoursValueLabel: UILabel!
     @IBOutlet weak var tasksValueLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
@@ -23,8 +26,28 @@ final class TechnicianCardCell: UITableViewCell {
         dotView.layer.cornerRadius = dotView.bounds.height / 2
         dotView.clipsToBounds = true
         
-        statusLabel.layer.cornerRadius = 14
+        statusLabel.layer.cornerRadius = 5
         statusLabel.clipsToBounds = true
+        
+        cardView.layer.cornerRadius = 16
+        cardView.layer.masksToBounds = false
+        cardView.layer.shadowOpacity = 0.12
+        cardView.layer.shadowRadius = 10
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 6)
+        
+        tasksViewCard.layer.cornerRadius = 14
+        tasksViewCard.layer.masksToBounds = true
+        tasksViewCard.layer.borderWidth = 1
+        tasksViewCard.layer.borderColor = UIColor.systemGray5.cgColor
+        
+        hoursViewCard.layer.cornerRadius = 14
+        hoursViewCard.layer.masksToBounds = true
+        hoursViewCard.layer.borderWidth = 1
+        hoursViewCard.layer.borderColor = UIColor.systemGray5.cgColor
+        
+        contentView.clipsToBounds = false
+        clipsToBounds = false
+        
     }
     
     override func layoutSubviews() {
