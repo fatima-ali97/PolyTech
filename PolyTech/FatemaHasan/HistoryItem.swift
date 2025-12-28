7f218ab821b48f9e0913440a94856cdfbc98fbb3
fatema-png <fatemazak62@gmail.com>
2025-12-28 14:17:23 +0300

    push

---
+++ PolyTech/FatemaHasan/HistoryItem.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct HistoryItem {
+    let id: String
+    let requestName: String
+    let itemName: String
+    let category: String
+    let quantity: Int
+    let location: String
+    let reason: String
+}
